2b348d2cd50e0041b35047d60df6b08f53b70fda
kirill_aleksandrov <kirill.alex.of@gmail.com>
2020-10-17 13:50:34 +0300

    update

--- task_5a+5b+5c/task_5b/index.php
+++ task_5a+5b+5c/task_5b/index.php
@@ -1,11 +1,48 @@
 <?php
-    $password = 437895;
-    
+    $password = "022222";
+    $flag = false;
 
-    for($i = 0; $i < 999999; $i++){
-        if($i == $password){
+    for ($i = 100000; $i < 999999; $i++){
+        if($i == (int)($password)){
             echo "Пароль: $i";
+            $flag = true;
             break;
+        }
+        else {
+            $flag = false;
+        }
+    }
+    if ($flag == false){
+        for ($i = 100000; $i < 999999; $i++){
+            $temp1 = "0".intdiv($i, 10);
+            $temp2 = "00".intdiv($i, 100);
+            $temp3 = "000".intdiv($i, 1000);
+            $temp4 = "0000".intdiv($i, 10000);
+            $temp5 = "00000".intdiv($i, 100000);
+            if ($password == $temp1){
+                echo "Пароль: $temp1";
+                break;
+            }
+            if ($password == $temp2){
+                echo "Пароль: $temp2";
+                break;
+            }
+            if ($password == $temp3){
+                echo "Пароль: $temp3";
+                break;
+            }
+            if ($password == $temp4){
+                echo "Пароль: $temp4";
+                break;
+            }
+            if ($password == $temp5){
+                echo "Пароль: $temp5";
+                break;
+            }
+            if($password == "000000"){
+                echo "Пароль: 000000";
+                break;
+            }
         }
     }
 ?>
