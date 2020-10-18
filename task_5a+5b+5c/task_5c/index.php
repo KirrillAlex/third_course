ec67ab9b677d6fc17939156b1e1a0248b6a5f6d4
kirill_aleksandrov <kirill.alex.of@gmail.com>
2020-10-18 12:03:37 +0300

    update

--- task_5a+5b+5c/task_5c/index.php
+++ task_5a+5b+5c/task_5c/index.php
@@ -4,14 +4,14 @@
     $pass2 = " ";
 
     $str = strval($pass1);
-    $first_pass1 = $str[0];
+    $first_pass1 = $str[2];
     $second_pass1 = $str[1];
-    $third_pass1 = $str[2];
+    $third_pass1 = $str[0];
 
     $str2 = strval($pass2Se);
-    $first_pass2Se = $str2[0];
+    $first_pass2Se = $str2[2];
     $second_pass2Se = $str2[1];
-    $third_pass2Se = $str2[2];
+    $third_pass2Se = $str2[0];
     
     if ($second_pass1 > 5){
         $pass2 = $first_pass2Se.$second_pass2Se.$third_pass2Se;
