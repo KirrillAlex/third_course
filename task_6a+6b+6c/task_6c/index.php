91fca969ad49f6f01b8616555bb1a2eef2ef715e
kirill_aleksandrov <kirill.alex.of@gmail.com>
2020-10-18 16:21:18 +0300

    update

--- task_6a+6b+6c/task_6c/index.php
+++ task_6a+6b+6c/task_6c/index.php
@@ -1,23 +1,17 @@
 <?php
-    $a = 'пять';
-    $b = 'восемь';
+$ip = 3232235777;
 
-    $num_mass = ['ноль', 'один', 'два', 'три', 'четыре', 'пять', 'шесть', 'семь', 'восемь', 'девять'];
+if (is_numeric($ip)) {
+    $part[0] = (int)($ip / 256 / 256 / 256);
+    $part[1] = (int)(($ip - $part[0] * 256 * 256 * 256) / 256 / 256);
+    $part[2] = (int)(($ip - $part[0] * 256 * 256 * 256 - $part[1] * 256 * 256) / 256);
+    $part[3] = $ip - $part[0] * 256 * 256 * 256 - $part[1] * 256 * 256 - $part[2] * 256;
+    $ip = "$part[0].$part[1].$part[2].$part[3]";
+} else {
+    $numb = explode(".", $ip);
+    $ip = $numb[0] * 256 * 256 * 256 + $numb[1] * 256 * 256 + $numb[2] * 256 + $numb[3];
+}
+echo $ip;
 
-    if (in_array($a, $num_mass) && in_array($b, $num_mass)){
-        $a = array_search($a, $num_mass);
-        $b = array_search($b, $num_mass);
-    }
-    else{
-        echo "Данные введены неверно!";
-    }
-
-    $result = $a + $b;
-    if ($result < 10){
-        echo"Результат: $result - маленькое число";
-    }
-    else {
-        echo"Результат: $result - большое число";
-    }
-    
 ?>
+
