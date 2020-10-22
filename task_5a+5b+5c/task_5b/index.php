7d2243530e6c896509efa1c8aebd189f9c3c8e24
kirill_aleksandrov <kirill.alex.of@gmail.com>
2020-10-22 20:33:11 +0300

    new update

--- task_5a+5b+5c/task_5b/index.php
+++ task_5a+5b+5c/task_5b/index.php
@@ -1,48 +1,14 @@
 <?php
-    $password = "022222";
-    $flag = false;
+    $password = "024012";
+    $try_pass = "";
 
-    for ($i = 100000; $i < 999999; $i++){
-        if($i == (int)($password)){
-            echo "Пароль: $i";
-            $flag = true;
+    for ($i = 0; $i < 999999; $i++){
+        $zero_num = strlen((string)$i);
+        $try_pass = str_repeat('0', 6 - $zero_num) . $i;
+        if ($password == $try_pass){
+            echo "Password is $try_pass";
             break;
         }
-        else {
-            $flag = false;
-        }
     }
-    if ($flag == false){
-        for ($i = 100000; $i < 999999; $i++){
-            $temp1 = "0".intdiv($i, 10);
-            $temp2 = "00".intdiv($i, 100);
-            $temp3 = "000".intdiv($i, 1000);
-            $temp4 = "0000".intdiv($i, 10000);
-            $temp5 = "00000".intdiv($i, 100000);
-            if ($password == $temp1){
-                echo "Пароль: $temp1";
-                break;
-            }
-            if ($password == $temp2){
-                echo "Пароль: $temp2";
-                break;
-            }
-            if ($password == $temp3){
-                echo "Пароль: $temp3";
-                break;
-            }
-            if ($password == $temp4){
-                echo "Пароль: $temp4";
-                break;
-            }
-            if ($password == $temp5){
-                echo "Пароль: $temp5";
-                break;
-            }
-            if($password == "000000"){
-                echo "Пароль: 000000";
-                break;
-            }
-        }
-    }
+    
 ?>
